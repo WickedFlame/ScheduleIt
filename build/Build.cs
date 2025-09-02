edcb4ca74b0633ff75f1448b11159f542a796ad6
Christian Walpen <c.walpen@gmx.net>
2025-09-02 21:47:49 +0200

    Next version

--- build/Build.cs
+++ build/Build.cs
@@ -43,7 +43,7 @@ class Build : NukeBuild
     readonly string ProjectName;
 
     [Parameter("Version to be injected in the Build")]
-    public string Version { get; set; } = $"1.0.0";
+    public string Version { get; set; } = $"1.0.1";
 
     [Parameter("The Buildnumber provided by the CI")]
     public int BuildNo = 35;
