6b7ecbb847c26b1bf8614f03b2459bbfdc20593d
Christian Walpen <c.walpen@gmx.net>
2025-09-02 18:28:23 +0200

    Flaky tests

--- src/Tests/ScheduleIt.Test/Diagnostics/MonitorTests.cs
+++ src/Tests/ScheduleIt.Test/Diagnostics/MonitorTests.cs
@@ -17,14 +17,14 @@ namespace ScheduleIt.Test.Diagnostics
         }
 
         [Test]
-        public void Monitor_Running_OnlyActive()
+        public async Task Monitor_Running_OnlyActive()
         {
             using var server = new TaskServer();
             server.Schedule(new TestTask(() => { Task.Delay(100).Wait(); }), s => s.Now().AndEvery(2).Seconds());
             server.Schedule(new TestTask(() => { Task.Delay(100).Wait(); }), s => s.Now());
             server.Schedule(new TestTask(() => { }), s => s.In(TimeSpan.FromMinutes(1)));
 
-            Task.Delay(100).Wait();
+            await Task.Delay(50);
 
             var monitor = new Monitor(server);
             monitor.Running.Should().HaveCount(2);
